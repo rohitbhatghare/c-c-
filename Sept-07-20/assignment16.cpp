2bae0600a47b8329dc451d43c3e9fda3d13d4940
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-08 10:17:23 -0700

    EvenNumber1To50

---
+++ Sept-07-20/assignment16.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"The even numbers 1 to 50 are:";
+    for(i=0;i<=50;i++)
+    {
+
+        if(i%2==0)
+        {
+
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
