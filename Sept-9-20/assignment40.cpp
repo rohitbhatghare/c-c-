4cbda3d3757146d49e51d8a9182da5c353ce496f
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-09 10:26:55 -0700

    CentimetersIntoInches

---
+++ Sept-9-20/assignment40.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+     double inch, cm;
+  cout<<"Enter the distance (only in cm):\n"<<endl;
+  cin>>cm;
+int  res = cm *0.3937;
+  cout<<"Distance of "<<cm<<"is ="<< "inches\t"<<res;
+    return 0;
+}
