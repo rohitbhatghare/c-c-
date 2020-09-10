0a849ad8615f728895ba658a026b69e5a88018ba
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-10 14:19:40 +0530

    Shifting User Data By Two Bits To The Left

---
+++ Sept-10-20/assignment42.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the Value \n";
+    cin>>a;
+    a=a>>2;
+    cout<<"The shifed Value is :\t"<<a;
+    return 0;
+}
