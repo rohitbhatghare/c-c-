be8ffcf9776519b16b66f3a615f6ddfa63759873
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-11 18:11:25 +0530

    Square And Cube Of 1-20 Numbers

---
+++ Sept-11-20/assignment58.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int m;
+
+
+    cout<<"Number"<<"\t"<<"square"<<"\t"<<"Cube"<<"\n";
+
+
+    for(m=0; m<=20; m++)
+        cout<<m<<"\t"<<m*m<<"\t"<<m*m*m<<"\n";
+
+    return 0;
+}
