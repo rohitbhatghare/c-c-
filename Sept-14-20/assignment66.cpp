118f72c7836694b55db44a0c55e6b7cec9baacb1
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-14 15:48:59 +0530

    Absolute Value Of n And 51

---
+++ Sept-14-20/assignment66.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 	//introduces namespace std
+int main()
+{
+    float b,a=51,n,res;
+
+cout<<"Enter The Number  'n' \n";
+cin>>n;
+
+
+    if (n>b)
+    {
+
+           res=(n-b)*3;
+    }
+    else if(n<a)
+        {
+   b=a-n;
+   cout<<"The Triple Of Two Same Numbers Are \n"<<res;
+        }
+
+
+cout<<"The Triple Of Two Diference Are \n"<<res;
+
+return 0;
+}
