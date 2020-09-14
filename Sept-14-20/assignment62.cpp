2f346760da3da4b1c61b4573e689fa1329afe5ae
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-14 14:03:15 +0530

    Print Result Of Specified Operation

---
+++ Sept-14-20/assignment62.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    float p,q,r;
+    p=-1+4*6;
+    q=(35+6)%7;
+    r=2+15/6*1-7%2;
+    cout<<"Values of operations are \n"<<p<<"\n"<<q<<"\n"<<r;
+
+    return 0;
+}
