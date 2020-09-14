3fd3b84602869e8f25968ca3732f239dd30434e2
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-14 15:50:32 +0530

    Two Integers and Return True IF One Of Them Is 30 Or Sum of Both Integers

---
+++ Sept-14-20/assignment67.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 	//introduces namespace std
+int main()
+{
+    float a,b,c,res;
+
+cout<<"Enter The First Integer\n";
+cin>>a;
+cout<<"Enter The Second Integer\n";
+cin>>b;
+
+c=a+b;
+
+if    (a==30 || b==30 || c==30)
+{
+    cout<<"True \n";
+
+}
+
+    else
+
+        cout<<"False \n";
+
+   return 0;
+}
