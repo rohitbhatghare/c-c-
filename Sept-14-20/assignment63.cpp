ec90cb0e4f4c2f655d33358350d7687e32bd3b83
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-14 14:04:08 +0530

    Multiplication Of Three Values Entered By user

---
+++ Sept-14-20/assignment63.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    float p,q,r,res;
+    cout<<"Enter the First Number \n";
+    cin>>p;
+    cout<<"Enter the First Number \n";
+    cin>>q;
+    cout<<"Enter the First Number \n";
+    cin>>r;
+    res=p*q*r;
+    cout<<"Multiplications of Three Numbers are \n"<<res;
+
+    return 0;
+}
