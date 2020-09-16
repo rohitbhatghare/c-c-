f23f251a230d2b3fb83b4d2b8a6a6d191d720025
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-16 15:53:32 +0530

    Check whether it is possible to add two integers to get the third integer from three given integers

---
+++ Sept-16-20/assignment86.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 	//introduces namespace std
+int main()
+{
+int a,b,c;
+    cout<<"Enter the Integer"<<endl;
+    cin>>a;
+    cout<<"Enter the Integer"<<endl;
+    cin>>b;
+
+
+
+
+
+if((c=a+b) || (a=b+c) ||(b=a+c))
+    {
+        cout<<"True";
+
+    }
+    else
+
+        cout<<"False";
+    return 0;
+}
+
