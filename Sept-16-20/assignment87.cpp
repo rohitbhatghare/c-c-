9bc8ca5404fd055cbe6b6fccbce25ea4a4825a47
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-16 15:57:04 +0530

    Check Y is greater than x , z is greater than y from three integers

---
+++ Sept-16-20/assignment87.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 	//introduces namespace std
+int main()
+{
+int x,y,z;
+    cout<<"Enter the X"<<endl;
+    cin>>x;
+    cout<<"Enter the Y"<<endl;
+    cin>>y;
+    cout<<"Enter the Y"<<endl;
+    cin>>z;
+
+
+
+
+
+
+
+
+if((x < y) && (y < z))
+    {
+        cout<<"Possible";
+
+    }
+    else
+
+        cout<<"Not Possible";
+    return 0;
+}
+
