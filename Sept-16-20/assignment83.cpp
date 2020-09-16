2658d16dfb5802f2e8b90b7323823b6101027925
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-16 15:47:14 +0530

    Check Given Number is multiple of 13 or not

---
+++ Sept-16-20/assignment83.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 	//introduces namespace std
+int main()
+{
+int a,b,n,rem;
+    cout<<"Enter the First Integer"<<endl;
+    cin>>n;
+
+
+
+if(n %13==0 || n % 13==1)
+    {
+        cout<<"1";
+
+    }
+    else
+
+        cout<<"0";
+    return 0;
+}
+
