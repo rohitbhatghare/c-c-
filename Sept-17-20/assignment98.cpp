907afe0891be85a7ea88bb51b224be04ed623d43
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-17 21:20:00 +0530

    Array contain 2 elemets check array does not contains 15 or 20

---
+++ Sept-17-20/assignment98.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+int main()
+{  int a[1000],n ,i;
+
+  cout<<"enter the elements of the array:\n";
+ for(i=0; i<=1; i++)
+ {
+     cin>>a[i];
+ }
+
+ if(a[0]!=12 && a[0]!=15 && a[1]!=12 && a[1]!=15 )
+ {
+
+ cout<<"True \n";
+ }
+  else
+  {
+      cout<<"False \n";
+  }
+  return 0;
+}
