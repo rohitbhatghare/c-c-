d10a767664bbe3cee1b443e3dd6676e17a5a9fee
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-22 12:43:09 +0530

    Example of member function inside the class

---
+++ Sept-22-20/AeOfCylinderOutside.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class area
+{
+public:
+    float a,h,r;
+    float pi=3.14;
+
+    int get()
+    {
+       cout<<"Enter the value of radius \n";
+       cin>>r;
+       cout<<"Enter the value of height \n";
+       cin>>h;
+    }
+    int cal();
+
+};
+ int area::cal()
+    {
+        return a=pi*r*r*h;
+    }
+int main()
+{
+    area c1;
+    c1.get();
+    c1.cal();
+    cout<<"Volume of cylinder is "<<c1.cal();
+
+    return 0;
+}
