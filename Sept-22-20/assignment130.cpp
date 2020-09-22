b9872ac503d26ab4970fcb35c84c4eb97cc1bc17
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-22 16:06:36 +0530

    Write a C program to read the value of an integer m and display the value of n is 1 when m is larger than 0, 0 when m is 0 and -1 when m is less than 0.

---
+++ Sept-22-20/assignment130.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+ int m,n;
+   cout<<"Input the  value of m :";
+   cin>>m;
+   if(m!=0)
+
+        if(m>0)
+
+
+         n=1;
+
+
+
+
+     else
+
+	n=-1;
+   else
+         n=0;
+
+  cout<<"The value of m = \t"<<m;
+    cout<<"The value of n = \t"<<n;
+    return 0;
+}
