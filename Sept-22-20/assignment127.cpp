8bb11f71a8cc22d07ab636ec984eaf143e66b879
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-22 15:51:07 +0530

    Write a C program to Convert Celcius to Farenheit

---
+++ Sept-22-20/assignment127.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,c;
+    cout<<"Enter the temperature In Celcius \n";
+    cin>>a;
+
+    c=(a * 9/5)+ 32;
+
+    cout<<"The Conversion In Farenheit is :\t"<<c;
+    return 0;
+}
