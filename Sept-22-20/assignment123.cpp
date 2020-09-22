1990c806aa1fb12b988af4c6236755562b132a1b
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-22 13:57:30 +0530

    Write a C program to that takes three numbers(x,y,z) as input and print the output of (x+y)·z and x·y + y·z.

---
+++ Sept-22-20/assignment123.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int a,b,c,d;
+   double res;
+   cout<<"Enter the first Number\n";
+   cin>>a;
+   cout<<"Enter the secomd Number\n";
+   cin>>b;
+   cout<<"Enter the first Number\n";
+   cin>>c;
+   cout<<"Enter the secomd Number\n";
+   cin>>d;
+   res=(a+b+c+d)/4;
+
+   cout<<"Average of 4 numbers are \t:"<<res;
+   return 0;
+}
