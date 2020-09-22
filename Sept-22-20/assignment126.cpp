74277eac7c69baa3f31c315c9d4ee827400d98db
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-22 15:50:13 +0530

    Write a C program that takes a number as input and then displays a rectangle of 3 columns wide and 5 rows tall using that digit.

---
+++ Sept-22-20/assignment126.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the number for recatangle\n";
+    cin>>a;
+    cout<<"Rectangle is:\n";
+    cout<<a<<a<<a<<endl;
+    cout<<a<<" "<<a<<endl;
+    cout<<a<<" "<<a<<endl;
+    cout<<a<<a<<a;
+}
