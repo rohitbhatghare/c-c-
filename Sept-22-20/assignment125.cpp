6c4b141f70f62078e6868f8ae444086b1ad815de
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-22 14:08:20 +0530

    Write a C program that takes an age (for example 20) as input and prints something as "You look older than 20

---
+++ Sept-22-20/assignment125.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int a,b,c,d;
+   double res;
+   cout<<"Enter the first Number\n";
+   cin>>a;
+   if(a<=20)
+   {
+       cout<<"You Look Younger \n";
+   }
+   else
+   {
+       cout<<"You Look Older \n";
+   }
+
+   return 0;
+}
