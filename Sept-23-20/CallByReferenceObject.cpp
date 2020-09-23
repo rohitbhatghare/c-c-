a1b90b5d9706d9c90e8382d4f8f9d72e4d1000ef
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-23 13:47:04 +0530

    Add files via upload

---
+++ Sept-23-20/CallByReferenceObject.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class one
+{
+public:
+    int a,b;
+    void sec()
+    {
+    a=2;
+    b=3;
+    }
+    void add(one &cob1)
+    {
+        cob1.a=12;
+        cob1.b=18;
+           }
+           void dis()
+           {
+               cout<<a<<b;
+           }
+};
+
+int main()
+{
+    one ob1;
+    ob1.sec();
+    ob1.dis();
+    ob1.add(ob1);
+    ob1.dis();
+
+    return 0;
+}
