6dcd2a5ba69f8a3c4e6f59633dbd6e774c2ac813
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-23 15:54:44 +0530

    C program to check whether an alphabet is a vowel or consonant

--- Sept-23-20/assignment140.cpp
+++ Sept-23-20/assignment140.cpp
@@ -8,17 +8,14 @@ int main()
     cin>>s_ch;
 
 
-    if((s_ch>='a' && s_ch<='z') || (s_ch>='A' && s_ch<='Z'))
-    {
-        cout<<"This is an Consonent.\n";
-    }
-    else if(s_ch=='a' || s_ch=='e' ||s_ch=='i' ||s_ch=='o' ||s_ch=='u' )
+
+    if(s_ch=='a' || s_ch=='e' ||s_ch=='i' ||s_ch=='o' ||s_ch=='u'||s_ch=='A' || s_ch=='E' ||s_ch=='I' ||s_ch=='O' ||s_ch=='U' )
     {
         cout<<"This is vowel.\n";
     }
     else
     {
-        cout<<"This is a special character.\n";
+        cout<<"This is a Consonent\n";
     }
 
     return 0;
