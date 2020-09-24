f8ef745c0b3c2e18d1296e9683e428f80646bf00
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-24 15:14:12 +0530

    Write c program to print 10 natural numbers

---
+++ Sept-24-20/assignment150.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i ,a;
+    cout<<"The 10 real numbers are \n";
+    for(i=0;i<10;i++)
+    {
+        cout<<i<<endl;
+    }
+    cout<<"\n\n\n\n";
+
+    return 0;
+}
