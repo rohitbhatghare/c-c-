6a96db495adb0680bd4397017a5fc4283811ef2e
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-25 13:41:26 +0530

    Write a C program to find the sum of first 10 natural numbers.

---
+++ Sept-25-20/assignment151.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i ,a=0;
+    cout<<"The 10 real numbers are \n";
+    for(i=1;i<=10;i++)
+    {
+        a=a+i;
+
+    }
+    cout<<a<<endl;
+    cout<<"\n\n\n\n";
+
+    return 0;
+}
