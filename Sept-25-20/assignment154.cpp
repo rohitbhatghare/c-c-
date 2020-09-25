52598987c1ea453d566d7693a60b0c0ad9fabf21
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-25 13:49:11 +0530

    Write a program in C to display the cube of the number upto a given integer.

---
+++ Sept-25-20/assignment154.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    float sum,avg;
+    cout<<"Enter the range for cube";
+    cin>>j;
+     for(i=0;i<=j;i++)
+    {
+       cout<<"Number is "<<i<<" and cube is "<<i*i*i<<endl;
+
+    }
+
+    return 0;
+}
