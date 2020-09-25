42f6859c6da1b51a193d795204e189d7e3d440c3
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-25 14:33:55 +0530

    Write a program in C to display the n terms of odd natural number and their sum

---
+++ Sept-25-20/assignment157.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    float sum,avg;
+    cout<<"Enter the Number for table";
+    cin>>j;
+     for(i=0;i<=10;i++)
+    {
+       cout<<2*i-1;
+       sum=sum+2*i-1
+
+    }
+    cout<<"The odd naturals numbers are "<<j<<" "<<sum;
+    return 0;
+}
+
