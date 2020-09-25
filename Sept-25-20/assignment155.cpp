8b325008e8abba3822e62421dcdb23b3ccc4bd3d
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-25 14:05:55 +0530

    Write a program in C to display the multiplication table of a given integer.

---
+++ Sept-25-20/assignment155.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    float sum,avg;
+    cout<<"Enter the Number for table";
+    cin>>j;
+     for(i=0;i<=10;i++)
+    {
+       cout<<j<<" X "<<i<<" = "<<j*i<<endl;
+
+    }
+
+    return 0;
+}
+
