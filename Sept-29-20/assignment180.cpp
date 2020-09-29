e34ef6afda9c778dd2d943438954bfbcdb66341d
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-09-30 00:29:43 +0530

    Write a program in C to find the sum of all elements of an array.

---
+++ Sept-29-20/assignment180.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+      int i,n,a[100],sum;
+
+    cout<<"Input the number of elements to store in the array :";
+   cin>>n;
+
+   cout<<"Input %d number of elements in the array :\n"<<n;
+   for(i=0;i<n;i++)
+      {
+	  cout<<"element : "<<i;
+	  cin>>a[i];
+	  }
+
+
+     cout<<"\n\nthe sum are :\n";
+    }
+
+    for(i=0; i<n; i++)
+    {
+        sum =sum + a[i];
+    }
+   cout<<"\n\n";
+   return 0;
+}
